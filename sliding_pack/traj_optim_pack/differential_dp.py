45141451eb441b94a9e735b47264a4387e4e58b5
jyp19 <jyp19@mails.tsinghua.edu.cn>
2022-11-24 23:34:46 +0800

    code for constrained ddp

--- sliding_pack/traj_optim_pack/differential_dp.py
+++ sliding_pack/traj_optim_pack/differential_dp.py
@@ -30,6 +30,7 @@ class buildDDPOptObj():
         
         self.W_x = cs.diag(cs.SX(configDict['W_x']))
         self.W_u = cs.diag(cs.SX(configDict['W_u']))
+        self.gamma_u = cs.diag(cs.SX(configDict['gamma_u']))
         
         # input constraints
         self.A_u = np.array([[ 1, 0, 0],
@@ -144,11 +145,29 @@ class buildDDPOptObj():
         __Q_mix[1:5, 1:5] = __Qxx
         __Q_mix[1:5, 5:] = __Qux.T; __Q_mix[5:, 1:5] = __Qux
         __Q_mix[5:, 5:] = __Quu
+        
+        self.Q_xx = cs.Function(
+            'Q_xx',
+            [self.dyn.x, self.dyn.u, self.dyn.beta, __Vx, __Vxx],
+            [__Qxx]
+        )
 
         self.Q_uu = cs.Function(
             'Q_uu',
             [self.dyn.x, self.dyn.u, self.dyn.beta, __Vx, __Vxx],
             [__Quu]
+        )
+        
+        self.Q_ux = cs.Function(
+            'Q_ux',
+            [self.dyn.x, self.dyn.u, self.dyn.beta, __Vx, __Vxx],
+            [__Qux]
+        )
+        
+        self.qx = cs.Function(
+            'qx',
+            [self.dyn.x, self.dyn.u, self.dyn.beta, __Vx, __Vxx],
+            [__qx]
         )
 
         self.qu = cs.Function(
@@ -169,8 +188,50 @@ class buildDDPOptObj():
             [0.5 * cs.dot(__dxu, cs.mtimes(__Q_mix, __dxu))]
         )
         
-    def project_feasible(self):
-        pass
+    def make_quadratic_approx(self):
+        """
+            make useful blocks storage of the matrix Q
+            ------output
+            Dict[List[]]
+        """
+        self.Q_block = {
+            'Q_xx': [],  # DM(4, 4)
+            'Q_uu': [],  # DM(3, 3)
+            'Q_ux': [],  # DM(3, 4)
+            'qx': [],  # DM(4, 1)
+            'qu': [],  # DM(3, 1)
+        }
+        
+    def project_feasible(self, k, uk, duk, dx):
+        """
+            get the inputs that do not violate constraints
+            in the forward propagate process
+        """
+        Q_xx = self.Q_block['Q_xx'][k]
+        Q_ux = self.Q_block['Q_ux'][k]
+        Q_uu = self.Q_block['Q_uu'][k]
+        qu = self.Q_block['qu'][k]
+        
+        H = Q_uu + self.gamma_u
+        G = Q_ux @ dx + qu - self.gamma_u @ duk
+        
+        A_u = cs.DM(self.A_u)  # DM(3, 3)
+
+        lb_a = self.lb_u - self.A_u @ uk
+        ub_a = self.ub_u - self.A_u @ uk
+        
+        qp = {'h': H.sparsity(),
+              'a': A_u.sparsity()}
+        S = cs.conic('S', 'qpoases', qp)
+        r = S(h=H, \
+              g=G, \
+              a=A_u, \
+              lba = lb_a, \
+              uba = ub_a)
+        duk_hat = r['x']  # suppose (3, 1)
+        
+        return uk + duk_hat
+        
 
     def coldboot_integration(self, x0, U0):
         """
@@ -196,16 +257,33 @@ class buildDDPOptObj():
             k_vec: (N, 3) is the set of feedforward vectors
             K_mat: (N, 3, 4) is the set of feedback matrices
         """
+        self.make_quadratic_approx()
+        
         VN = self.VN([X[:, -1]]).toarray()[0, 0]  # double
         V = VN  # value function
         Vx, Vxx = np.zeros((4, 1)), np.zeros((4, 4))
+        self.Vx_tensor, self.Vxx_tensor = np.zeros((self.TH, 4, 1)), np.zeros((self.TH, 4, 4))
+        self.k_vec, self.K_mat = np.zeros((self.TH, 3)), np.zeros((self.TH, 3, 4))
+        
         for k in range(self.TH - 1, -1, -1):
             if k == self.TH - 1:
                 Vx = self.VNx([X[:, -1]]).toarray()  # (4, 1)
                 Vxx = self.VNxx([X[:, -1]]).toarray()  # (4, 4)
 
-            Q_uu = self.Q_uu(X[:, k+1], U[:, k], self.beta, Vx, Vxx)  # DM(3, 3)
-            qu = self.qu(X[:, k+1], U[:, k], self.beta, Vx, Vxx)  # DM(3, 1)
+            # calculate matrix values
+            variables = X[:, k], U[:, k], self.beta, Vx, Vxx
+
+            Q_xx = self.Q_xx(variables)
+            Q_ux = self.Q_ux(variables)
+            Q_uu = self.Q_uu(variables)
+            qx = self.qx(variables)
+            qu = self.qu(variables)
+            
+            self.Q_block['Q_xx'].insert(0, Q_xx)
+            self.Q_block['Q_ux'].insert(0, Q_ux)
+            self.Q_block['Q_uu'].insert(0, Q_uu)
+            self.Q_block['qx'].insert(0, qx)
+            self.Q_block['qu'].insert(0, qu)
 
             A_u = cs.DM(self.A_u)  # DM(3, 3)
 
@@ -227,26 +305,37 @@ class buildDDPOptObj():
             eps = 1e-8
             grad = qu.toarray() + Q_uu.toarray() @ du
             c_x = np.where(np.bitwise_or(np.bitwise_and(np.abs(self.A_u @ U[:, k] - lb_a) < eps, self.A_u @ grad > 0), \
-                                         np.bitwise_and(np.abs(self.A_u @ U[:, k] - ub_a) < eps, self.A_u @ grad < 0)) == 1)
+                                         np.bitwise_and(np.abs(self.A_u @ U[:, k] - ub_a) < eps, self.A_u @ grad < 0)) == 0)
+            invQ_uu = np.linalg.inv(Q_uu.toarray())
+            invQ_uu[c_x, :] = 0
             
+            self.k_vec[k, :] = np.array(du)
+            self.K_mat[k, ...] = -invQ_uu @ Q_ux.toarray()
+            
+            # propagate value
+            V += 0.5 * self.k_vec[k, :] @ Q_uu.toarray() @ self.k_vec[k, :]
+            Vx = qx - self.K_mat[k, ...].T @ Q_uu.toarray() @ self.k_vec[k, :]
+            Vxx = Q_xx - self.K_mat[k, ...].T @ Q_uu.toarray() @ self.K_mat[k, ...]
+            self.Vx_tensor[k, :], self.Vxx_tensor[k, ...] = Vx, Vxx
+            
+        return self.k_vec, self.K_mat
 
-
-
-    def forward_integration(self, X, U, k, K):
+    def forward_integration(self, X, U):
         """
             X: (4, N) is the array of current states
             U: (3, N) is the array of current inputs
             k: (3, 1) is the feedforward vector
             K: (3, 4) is the feedback matrix
         """
-        X_hat = np.zeros((4, self.TH + 1))
+        X_hat, U_hat = np.zeros((4, self.TH + 1)), np.zeros((4, self.TH))
         X_hat[:, 0] = X[:, 0]  # (4, 1)
         for k in range(0, self.TH):
-            uk_hat = U[:, k] + k + K @ (X_hat[:, k] - X[:, k])
-            uk_hat = self.project_feasible(uk_hat)  # (3, 1)
-            X_hat[:, k+1] = self.f_xu(X_hat[:, k], uk_hat).toarray()
+            k_vec, K_mat = self.k_vec[k, :], self.K_mat[k, ...]
+            dx = X_hat[:, k] - X[:, k]
+            U_hat[:, k] = self.project_feasible(k, U[:, k], k_vec + K_mat @ dx)  # (3, 1)
+            X_hat[:, k+1] = self.f_xu(X_hat[:, k], U_hat[:, k]).toarray()
         
-        return X_hat
+        return X_hat, U_hat
         
     def solve_constrained_ddp(self, x_init, U_init):
         """
